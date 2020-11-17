a7e32a99faa6a9ef7b5cdcd76172846d52e7792e
Ivan V. Dimitrov <ivan@stageai.tech>
2020-11-17 10:23:37 +0200

    Clear some precompiled binaries

--- zariba/PollyShakeMaker/cocos2d/cocos/platform/android/java/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/vanjiii/AndroidDev/Projects/PollyShakeMaker/cocos2d/cocos/platform/android/java/bin/AndroidManifest.xml : \
-/home/vanjiii/AndroidDev/Projects/PollyShakeMaker/cocos2d/cocos/platform/android/java/AndroidManifest.xml \
--- zariba/PollyShakeMaker/proj.android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/vanjiii/AndroidDev/Projects/PollyShakeMaker/proj.android/bin/AndroidManifest.xml : \
-/home/vanjiii/AndroidDev/Projects/PollyShakeMaker/proj.android/AndroidManifest.xml \
